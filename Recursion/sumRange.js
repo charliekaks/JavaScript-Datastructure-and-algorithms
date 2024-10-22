8dc888d7de41dde6aad9c8afcf99c967e4006826
charliekaks <kaksunchained@gmail.com>
2024-10-22 12:24:32 +0300

    Adding a recurcive function for factorial

--- Recursion/sumRange.js
+++ Recursion/sumRange.js
@@ -3,3 +3,9 @@ function sumRange(num){
     if (num === 1 )return 1;
     return num + sumRange(num-1);
 }
+
+//A function that returns the factorial of a number given the number as an input
+function factorial(num){
+    if(num === 1) return 1
+    return num * factorial(num-1);
+}
