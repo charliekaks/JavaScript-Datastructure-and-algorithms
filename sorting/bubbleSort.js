34960a6c7137e33236d09b70b6ac6ca1ad0cc084
charliekaks <kaksunchained@gmail.com>
2024-10-28 13:09:28 +0300

    An optimised version of bubble sort for when the array is almost sorted

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,6 +1,4 @@
-function swap(i,j){
-    
-}
+//First implementation of bubble sort
 function bubbleSort(array){
     for(let i = array.length; i>0; i--){
         
@@ -18,3 +16,26 @@ function bubbleSort(array){
     return array;
 }
 bubbleSort([9,12,7,88,10,16])
+
+
+//An optimised version of bubble sort
+
+//First implementation of bubble sort
+function bubbleSort(array){
+    for(let i = array.length; i>0; i--){
+        noSwaps = true;
+        for(let j= 0; j< i-1;j++){
+            console.log(array);
+            if(array[j] > array[j+1]){
+                let temp = array[j];
+                array[j]= array[j+1];
+                array[j+1] = temp;
+                noSwaps = false;
+            }
+        }
+        console.log("one pass complete");
+        if(noSwaps){ break}
+    }
+    console.log(array)
+    return array;
+}
