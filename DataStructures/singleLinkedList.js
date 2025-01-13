85542ed088ac20745e82b9db38edcba7f7a3f49e
charliekaks <kaksunchained@gmail.com>
2025-01-13 10:00:00 +0300

    Added more methods to the singlylist class

--- DataStructures/singleLinkedList.js
+++ DataStructures/singleLinkedList.js
@@ -71,6 +71,40 @@ class singlyLinkedList {
         return this;
         
     }
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    set(index, val){
+        var foundNode = this.get(index);
+        if(foundNode){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index, val){
+        if (index === 0) !!this.unshift(val)
+        if (index === this.length) !!this.push(val)
+        if (index < 0 || index > this.length) return false;
+
+        let node = new Node(val);
+        let prev = this.get(index-1);
+        let current = prev.next;
+        prev.next = node;
+        node.next = current;
+        this.length++;
+        return true
+    }
+    remove(index){
+        
+    }
 }
 let list = new singlyLinkedList();
 list.push("hey");
@@ -80,4 +114,5 @@ list.push("boy");
 list.pop();
 list.shift();
 list.unshift("1000");
+list.insert(2, "insert")
 console.log(list);
