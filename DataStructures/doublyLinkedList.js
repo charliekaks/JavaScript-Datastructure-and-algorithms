877154bc02a8fd5bd617df3e8f2d40cd201c2585
charliekaks <kaksunchained@gmail.com>
2025-01-16 17:07:50 +0300

    Finishing up a doublylinked list implementation.

--- DataStructures/doublyLinkedList.js
+++ DataStructures/doublyLinkedList.js
@@ -66,14 +66,72 @@ class DoublyLinkedList{
         if (this.length === 1 ){ 
             this.head = null;
             this.tail = null;
+        } else{
+            let newHead = this.head.next;
+            newHead.prev = null;
+            removed.next = null;
+            this.head = newHead;
         }
-
-        let newHead = this.head.next;
-        newHead.prev = null;
-        removed.next = null;
-        this.head = newHead;
         this.length--;
         return removed;
+    }
+    get(index){
+        if(index < 0 || index >= this.length) return null;
+        var count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+    set(index, val){
+        var foundNode = this.get(index);
+        if(foundNode != null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+
+        var newNode = new Node(val);
+        var beforeNode = this.get(index-1);
+        var afterNode = beforeNode.next;
+        
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
+    remove(index){
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        var removedNode = this.get(index);
+        var beforeNode = removedNode.prev;
+        var afterNode = removedNode.next;
+        beforeNode.next = afterNode;
+        afterNode.prev = beforeNode;
+        // removedNode.prev.next = removedNode.next;
+        // removedNode.next.prev = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+        this.length--;
+        return removedNode;
     }
 }
 
@@ -83,4 +141,6 @@ list.push("822");
 list.push("about");
 list.push("them");
 list.pop();
+list.shift();
+list.unshift("6")
 console.log(list);
