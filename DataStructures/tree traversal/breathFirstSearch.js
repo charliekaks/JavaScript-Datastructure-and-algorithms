3b02026fc78e8373bad65ffb77a85333dc6fe176
charliekaks <kaksunchained@gmail.com>
2025-02-12 16:30:02 +0300

    Iterative approch to doing breath first search

---
+++ DataStructures/tree traversal/breathFirstSearch.js
@@ -0,0 +1,13 @@
+function breathFirstSearch(){
+    let node = this.root,
+                queue = [],
+                data = [];
+    queue.push(node);
+    while(queue.length){
+        node = queue.shift();
+        data.push(node);
+        if(node.left) queue.push(node.left)
+        if(node.right) queue.push(node.right)
+    }
+return data;
+}
