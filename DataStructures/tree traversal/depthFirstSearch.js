014518dbe645cc3389d733d84ce49004aae17276
charliekaks <kaksunchained@gmail.com>
2025-02-14 15:31:50 +0300

    tree traversal in depth first search in order sequence

--- DataStructures/tree traversal/depthFirstSearch.js
+++ DataStructures/tree traversal/depthFirstSearch.js
@@ -13,9 +13,20 @@ function traverse(node,values){
 function depthFirstSearchPostOrder(){
     let data = [];
     function traverse(node){
-        if(node.left) traverse(node.left,values)
-        if(node.right) traverse(node.right,values)
+        if(node.left) traverse(node.left)
+        if(node.right) traverse(node.right)
         data.push(node.value);   
+    }
+    traverse(root)
+    return data;
+}
+
+function depthFirstSearchInOrder(){
+    let data = [];
+    function traverse(node){
+        if(node.left) traverse(node.left)
+        data.push(node.value); 
+        if(node.right) traverse(node.right) 
     }
     traverse(root)
     return data;
