a7b32bfc2a35b1366408776054c3148ffc3b51c9
charliekaks <kaksunchained@gmail.com>
2025-02-17 16:45:04 +0300

    Adding insert method to the maxbinaryheap class.

--- DataStructures/binaryHeap.js
+++ DataStructures/binaryHeap.js
@@ -1,10 +1,21 @@
-class BinaryHeap{
+class MaxBinaryHeap{
     constructor(){
         this.values = [];
     }
 
     insert(value){
         this.values.push(value);
-        let current
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;}
     }
 }
